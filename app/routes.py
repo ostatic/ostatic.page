198e681e8dc3fde41f45f21ea4c0d861bbb8bf43
ostatic <aleksey@cedato.com>
2019-02-22 20:35:02 +0200

    j2

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,15 @@
 
 from app import app
 from flask import Flask, request, jsonify
+from flask import render_template
 import base64, hmac, hashlib
 import ssl
 import urllib
+
+@app.route('/')
+def index():
+    return render_template('core.html')
+
 
 
 @app.route('/healthcheck')
